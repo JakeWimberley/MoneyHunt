bb830bbb1ab17d1d2962e1eb4f033a5f9db40613
Jake or Katie Wimberley <jakeandkatie@Jake-or-Katie-Wimberleys-Mac-mini.local>
2014-07-20 19:56:53 -0400

    Initial Commit

---
+++ BONB/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BONB
+//
+//  Created by Jake or Katie Wimberley on 7/20/14.
+//  Copyright (c) 2014 Jake Wimberley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
